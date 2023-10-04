7369172b2e3f7e4091d2b533cf85c0b599ca3bb6
Rafa ielo <140087246+ielito@users.noreply.github.com>
2023-10-04 00:49:10 -0300

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MongoDB_Code.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
